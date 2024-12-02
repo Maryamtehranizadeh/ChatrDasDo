f598a7d82ec5442185feb1210cc8f82fa7347db2
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-12-02 22:57:11 +0000

    adding property details

--- src/components/ItemFormExtraProperties.jsx
+++ src/components/ItemFormExtraProperties.jsx
@@ -1,3 +1,4 @@
+import { Label } from "@headlessui/react";
 import React from "react";
 
 function ItemFormExtraProperties({ data, categoryId }) {
@@ -8,8 +9,36 @@ function ItemFormExtraProperties({ data, categoryId }) {
   );
   console.log(selectedCategory.extra_properties);
 
-  
-  return <div>ItemFormExtraProperties</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "start",
+        alignItems: "start",
+      }}
+    >
+      {selectedCategory.extra_properties.map((extra_property) => (
+        <div
+          key={extra_property.name}
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "start",
+          }}
+        >
+          <label htmlFor={extra_property.name} style={{ width: "20%" }}>
+            {extra_property.name} {!extra_property.required && "(Optional)"}
+          </label>
+          <input
+            type={extra_property.type}
+            name={extra_property.name}
+            id={extra_property.name}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ItemFormExtraProperties;
