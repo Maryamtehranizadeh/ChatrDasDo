5934422a49eb22c19ff5035cbde2120222503ed3
maryam tehrani <maryamtehranizadeh@yahoo.com>
2025-02-15 15:48:17 +0000

    pass reset tw res

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,9 +3,9 @@ function Footer() {
     "text-lg bg-[var(--primary-color)] text-[var(--secondary-color)] px-4 py-2 rounded-md hover:opacity-80 w-full md:w-auto hover:border-[var(--secondary-color)]";
 
   return (
-    <footer className=" text-center p-10 bg-[var(--primary-color)] text-[var(--secondary-color)]">
+    <footer className=" flex flex-col text-center p-10 bg-[var(--primary-color)] text-[var(--secondary-color)]">
       <p>Developed by Mary with ❤</p>
-      <div>
+      <div className="flex flex-row m-auto">
         <button className={buttonClass}>Ins</button>
         <button className={buttonClass}>Tel</button>
         <button className={buttonClass}>FB</button>
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,4 +1,3 @@
-import styles from "./ResetPassword.module.css";
 import { useState } from "react";
 
 function ResetPassword() {
@@ -6,20 +5,34 @@ function ResetPassword() {
   return (
     <div>
       {verified ? (
-        <form className={styles.form}>
-          <h1>Create new password</h1>
+        <form className="flex flex-col max-w-[600px] m-auto p-[40px]">
+          <h1 className="text-lg md:text-2xl flex-wrap text-center">
+            Create new password
+          </h1>
           <label htmlFor="password">New password</label>
           <input id="password" type="password" />
           <label htmlFor="confirm_password">Confirm your password</label>
           <input id="confirm_password" type="confirm_password" />
-          <button type="submit">Set New Password</button>
+          <button
+            className="w-max px-4 text-nowrap m-auto my-3 md:w-1/2"
+            type="submit"
+          >
+            Set New Password
+          </button>
         </form>
       ) : (
-        <form className={styles.form}>
-          <h1>To reset your password, please enter your email</h1>
+        <form className="flex flex-col max-w-[600px] m-auto p-[40px]">
+          <h1 className="text-lg md:text-2xl flex-wrap text-center">
+            To reset your password, please enter your email
+          </h1>
           <label htmlFor="email">Your Email</label>
           <input id="email" type="email" />
-          <button type="submit">Send Verification Link</button>
+          <button
+            className="w-max px-4 text-nowrap m-auto my-3 md:w-1/2"
+            type="submit"
+          >
+            Send Verification Link
+          </button>
         </form>
       )}
     </div>
